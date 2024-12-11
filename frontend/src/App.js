7fa845d4c0434adc8ed04e5369f47c0573ca17df
Hind Ehr <ehrhind@gmail.com>
2024-12-11 14:50:38 +0100

    tp react

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CompteList from './component/CompteList';
+import CompteForm from './component/CompteForm';
+
+function App() {
+return (
+<div>
+<CompteForm />
+<CompteList />
+</div>
+);
+}
+export default App;
+
+
